2fa7413e5d07c55f202bcb741afb86da6184d8c5
rylandavis9 <165442600+rylandavis9@users.noreply.github.com>
2025-02-22 15:10:51 -0600

    updated more margins

--- website/app/calendar/page.tsx
+++ website/app/calendar/page.tsx
@@ -10,7 +10,7 @@ export default function Calendar() {
         </Navbar>
         <a href="https://calendar.google.com/calendar/u/0?cid=Y18yMmVhZTQ1MzRlZDZkNmQ2ZDVmODU2ODVhNmU2NGJmMWY5OGIwYzhmMjY1MDZhOGQ2MjkzMGQxNzYyNDhmZmIwQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20" target="_blank" className="underline mx-10">Get the sharable calendar link here!</a>
         <div className="flex place-content-center">
-          <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FChicago&showPrint=0&src=Y18yMmVhZTQ1MzRlZDZkNmQ2ZDVmODU2ODVhNmU2NGJmMWY5OGIwYzhmMjY1MDZhOGQ2MjkzMGQxNzYyNDhmZmIwQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&color=%2333B679" className="flex  w-full h-screen max-md:p2 md:p-12"></iframe>
+          <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FChicago&showPrint=0&src=Y18yMmVhZTQ1MzRlZDZkNmQ2ZDVmODU2ODVhNmU2NGJmMWY5OGIwYzhmMjY1MDZhOGQ2MjkzMGQxNzYyNDhmZmIwQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&color=%2333B679" className="flex  w-full h-screen p2 md:p-12"></iframe>
         </div>
       </main>
   
