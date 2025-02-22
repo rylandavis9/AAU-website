5c4b1e0b412b5c9497fde4aa44e001b3d0d8f3ad
rylandavis9 <165442600+rylandavis9@users.noreply.github.com>
2025-02-22 13:40:00 -0600

    Made Responsive

--- website/app/importantdates/page.tsx
+++ website/app/importantdates/page.tsx
@@ -8,7 +8,7 @@ export default function ImportantDates() {
         </Navbar>
         <h1 className="m-2 text-pretty text-4xl self-center font-bold"> <br></br>Important Dates<br></br> </h1>
         <p className="m-2 text-pretty text-lg font-bold">
-        <a href="/home" >Monday, March 10th 5:30pm (2025 Parent Meeting)<br></br><br></br></a>
+        <a href="/" >Monday, March 10th 5:30pm (2025 Parent Meeting)<br></br><br></br></a>
         <a href="/calendar" >Saturday, April 26th Girard Meet<br></br><br></br></a>
         <a href="/calendar" >Saturday, May 3rd Neosho Meet<br></br><br></br></a>
         <a>Monday, May 19th Team Pictures <a href="https://www.canva.com/design/DAGeV0mKrG4/Fft2ZSL1_CLZNX3hVx7xHg/view?utm_content=DAGeV0mKrG4&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hd9249e7883" target="_blank" className="underline">Picture Order Form</a> <br></br><br></br></a>
