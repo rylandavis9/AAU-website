7f1a3a30f713b35f5b27bb91684e8aacd1a2c6b2
rylandavis9 <165442600+rylandavis9@users.noreply.github.com>
2025-02-22 13:26:53 -0600

    updated picture form link

--- website/app/importantdates/page.tsx
+++ website/app/importantdates/page.tsx
@@ -11,7 +11,7 @@ export default function ImportantDates() {
         <a href="/home" >Monday, March 10th 5:30pm (2025 Parent Meeting)<br></br><br></br></a>
         <a href="/calendar" >Saturday, April 26th Girard Meet<br></br><br></br></a>
         <a href="/calendar" >Saturday, May 3rd Neosho Meet<br></br><br></br></a>
-        <a>Monday, May 19th Team Pictures <a href="" target="_blank" className="underline">Picture Order Form</a> <br></br><br></br></a>
+        <a>Monday, May 19th Team Pictures <a href="https://www.canva.com/design/DAGeV0mKrG4/Fft2ZSL1_CLZNX3hVx7xHg/view?utm_content=DAGeV0mKrG4&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hd9249e7883" target="_blank" className="underline">Picture Order Form</a> <br></br><br></br></a>
         <a href="/calendar" >Districts TBA<br></br><br></br></a>
         <a href="/calendar" >Regionals TBA @ Springfield, Mo<br></br><br></br></a>
         </p>
