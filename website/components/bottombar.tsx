afe6f4de59df42e1daea0e7488374a0a6f754fc5
rylandavis9 <165442600+rylandavis9@users.noreply.github.com>
2025-02-26 10:04:25 -0600

    Update bottombar.tsx

--- website/components/bottombar.tsx
+++ website/components/bottombar.tsx
@@ -9,7 +9,7 @@ interface Props {
 
   const Bottombar = ({children, ...props}: Props) => {
     return (
-      <div className="flex self-stretch flex-1 w-full min-h-10 bg-zinc-950">
+      <div className="flex self-stretch flex-1 w-full max-h-5 min-h-5 bg-zinc-950">
         <div className="grow shrink m-2 text-med font-semibold text-lime-500"></div>
       </div>       
     );
